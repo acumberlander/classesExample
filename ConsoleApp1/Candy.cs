a6eec1dcb4fe9693e0b568bb1b017983b4d60096
Austin <austincumberlander@gmail.com>
2019-03-02 14:00:54 -0600

    class examples

---
+++ ConsoleApp1/Candy.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace ClassesExample
+{
+    class Candy
+    {
+        //Properties
+        public string Flavor { get; set; }
+        public int Rating { get; private set; }
+        public string Name { get; set; }
+        public CandyType Type { get; set; }
+
+        //Fields
+        const int MinRatingValue = 1;
+        const int MaxRatingValue = 10;
+
+        //Constructor
+        public Candy(string flavor, string name, CandyType type)
+        {
+            Flavor = flavor;
+            Name = name;
+            Type = type;
+        }
+
+        //Methods
+        // void is a return type. with a void, you can't return anything.
+        public void SetRating(int userRating)
+        {
+            // needs to be bwtween 1-10
+            if (userRating >= MinRatingValue && userRating <= MaxRatingValue)
+            {
+                Rating = userRating;
+            }
+            else
+            {
+                throw new Exception($"You are dumb. Ratings are {MinRatingValue}-{MaxRatingValue}, you moron.");
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"This is {Type} {Name}, with a(n) {Flavor} flavor, rated {Rating} with a review of it's aight.";
+        }
+    }
+
+    enum CandyType
+    {
+        Chocolate,
+        HardCandy,
+        Carmel,
+        Sour,
+        Stretchy,
+        Nut
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ClassesExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var taffy = new Candy();
+            //taffy.Flavor = "Orange";
+            //taffy.Name = "Orange Saltwater Taffy";
+            //taffy.Rating = 5;
+            //taffy.Type = CandyType.Stretchy;
+
+            var taffy = new Candy("Orange", "Orange Saltwater Taffy", CandyType.Stretchy);
+            taffy.SetRating(4);
+
+            Console.WriteLine(taffy);
+
+            Console.WriteLine(taffy.Rating);
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
